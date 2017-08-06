b1851ee93b06194275f6aaa5c523e0118619f495
Ryan Klatt <rpklatt@msn.com>
2017-08-05 23:21:00 -0600

    Added on and off buttons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private flashlight: Flashlight) {
 
   }
 
+  lightOn(){
+  	this.flashlight.switchOn();
+  	var lightOn = document.getElementById('lightOn');
+  	var lightOff = document.getElementById('lightOff');
+    if (lightOn.style.display === 'none') {
+        lightOn.style.display = 'block';
+    } else {
+        lightOn.style.display = 'none';
+        lightOff.style.display = 'block';
+    }
+  }
+  lightOff(){
+  	this.flashlight.switchOff();
+  	var lightOn = document.getElementById('lightOn');
+  	var lightOff = document.getElementById('lightOff');
+    if (lightOff.style.display === 'none') {
+        lightOff.style.display = 'block';
+    } else {
+        lightOff.style.display = 'none';
+        lightOn.style.display = 'block';
+    }
+  }
 }
